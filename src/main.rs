5beff57cf2dccf4e90a59bf9704e188f06da287d
Peanuts <849421294godw@gmail.com>
2022-05-31 18:01:14 +0000

    add example for header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{
+    get, handler, http::StatusCode, listener::TcpListener, middleware::Tracing, EndpointExt,
+    IntoResponse, Response, Route, Server,
+};
+
+static JSON: &str = r#"
+{
+    "code":"0",
+    "msg":"ok",
+    "data":[]
+}
+"#;
+
+#[handler]
+fn hello() -> Response {
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .status(StatusCode::OK)
+        .body(JSON.to_string())
+        .into_response()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Route::new().at("/hello", get(hello)).with(Tracing);
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .name("hello-world")
+        .run(app)
+        .await
 }
